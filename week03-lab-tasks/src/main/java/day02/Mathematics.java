2a7f72322a5150aeffee613dbdea247753a9457f
Imre-Vaczi <imre.vaczi@gmail.com>
2021-11-01 16:55:04 +0100

    Mathematics class updated

--- week03-lab-tasks/src/main/java/day02/Mathematics.java
+++ week03-lab-tasks/src/main/java/day02/Mathematics.java
@@ -11,14 +11,13 @@ public class Mathematics {
         }
         else{
             int divisors = 0;
-            for (int i = 2; i < number; i++){
+            for (int i = 2; i < (number/2)+1; i++){
                   if (number % i == 0){
                       divisors++;
                   }
               }return !(divisors >0);
         }
     }
-
     public static void main(String[] args) {
         Mathematics mathematics = new Mathematics();
         for (int i = 0; i < 14; i++){
