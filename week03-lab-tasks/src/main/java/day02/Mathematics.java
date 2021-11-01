e65ae8ad5b082de5e4ce3cf02d51b53d2c1a8964
Imre-Vaczi <imre.vaczi@gmail.com>
2021-11-01 15:57:31 +0100

    Mathematics class updated

---
+++ week03-lab-tasks/src/main/java/day02/Mathematics.java
@@ -0,0 +1,28 @@
+package day02;
+
+public class Mathematics {
+
+    public boolean isPrime(int number){
+        if (number == 1 | number == 0){
+            return false;
+        }
+        else if (number == 2 | number == 3){
+            return true;
+        }
+        else{
+            int divisors = 0;
+            for (int i = 2; i < number; i++){
+                  if (number % i == 0){
+                      divisors++;
+                  }
+              }return !(divisors >0);
+        }
+    }
+
+    public static void main(String[] args) {
+        Mathematics mathematics = new Mathematics();
+        for (int i = 0; i < 14; i++){
+            System.out.println("Is " + i +" a prime number? Solution: " + mathematics.isPrime(i));
+        }
+    }
+}
